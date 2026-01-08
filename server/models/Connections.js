136737a06f262b459ac30fe2ea78da523c1a9d76
Zubair Abid <zubairabid54@gmail.com>
2026-01-08 09:49:59 +0500

    create connections schema

---
+++ server/models/Connections.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectionSchema = new mongoose.Schema(
+  {
+    from_user_id: { type: String, required: true, ref: "User" },
+    to_user_id: { type: String, required: true, ref: "User" },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+const Connection = mongoose.model("Connection", connectionSchema);
+
+export default Connection;
