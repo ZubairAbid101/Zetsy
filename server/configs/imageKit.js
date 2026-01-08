7e99fc323875fe19e78e6c2a0a702ad54f002094
Zubair Abid <zubairabid54@gmail.com>
2026-01-08 12:07:28 +0500

    fix env imports

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,9 +1,9 @@
 import ImageKit from "@imagekit/nodejs";
 
 const imageKitClient = new ImageKit({
-  urlEndpoint: process.env["IMAGEKIT_URL_ENDPOINT"],
-  publicKey: process.env["IMAGEKIT_PUBLIC_KEY"],
-  privateKey: process.env["IMAGEKIT_PRIVATE_KEY"],
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
 });
 
 export default imageKitClient;
