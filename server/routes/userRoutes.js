e06a57b3d180fe30cd0b3ca9d815c368f61e2e9f
Zubair Abid <zubairabid54@gmail.com>
2026-01-08 09:45:30 +0500

    create api routes

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,31 @@
+import express from "express";
+import {
+  discoverUsers,
+  followUser,
+  getUserData,
+  unfollowUser,
+  updateUserData,
+} from "../controllers/userControllers.js";
+import { protect } from "../middleware/auth.js";
+import { upload } from "../configs/multer.js";
+
+const userRouter = express.Router();
+
+// Routes
+userRouter.get("/data", protect, getUserData);
+
+userRouter.post(
+  "/update",
+  protect,
+  upload.fields([
+    { name: "profile", maxCount: 1 },
+    { name: "cover", maxCount: 1 },
+  ]),
+  updateUserData
+);
+
+userRouter.post("/discover", protect, discoverUsers);
+userRouter.post("/follow", protect, followUser);
+userRouter.post("/unfollow", protect, unfollowUser);
+
+export default userRouter;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
 import { serve } from "inngest/express";
-import {inngest, functions} from './inngest/index.js'
-import connectDB from './configs/db.js';
-import { clerkMiddleware } from '@clerk/express'
+import { inngest, functions } from "./inngest/index.js";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 
@@ -14,16 +15,17 @@ await connectDB();
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
-app.use('/api/inngest', serve({ client: inngest, functions }));
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/user", userRouter);
 
 // Start the server
 app.listen(process.env.PORT || 4000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+  console.log(`Server is running on port ${process.env.PORT || 4000}`);
 });
