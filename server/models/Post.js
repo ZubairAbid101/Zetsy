69c84e0b0d692d37a30cf3bb21e285fb1c1648c1
Zubair Abid <zubairabid54@gmail.com>
2026-01-11 20:47:20 +0500

    create Post model in mongodb

---
+++ server/models/Post.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    user: { type: String, ref: "User", required: true },
+    content: { type: String, required: true },
+    image_urls: [{ type: String }],
+    post_type: {
+      type: String,
+      enum: ["text", "image", "text_with_image"],
+      default: "text",
+      required: true,
+    },
+    likes: [{ type: String, ref: "User" }],
+  },
+  { timestamps: true, minimize: false }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
