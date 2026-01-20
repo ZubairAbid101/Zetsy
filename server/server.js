987eee3ded37053c77310980bcff39c853267095
Zubair Abid <zubairabid54@gmail.com>
2026-01-20 20:59:20 +0500

    add message routes to server

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import { clerkMiddleware } from "@clerk/express";
 import userRouter from "./routes/userRoutes.js";
 import postRouter from "./routes/postRoutes.js";
 import storyRouter from "./routes/storyRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
 
 const app = express();
 
@@ -28,6 +29,7 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/user", userRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/stories", storyRouter);
+app.use("/api/messages", messageRouter);
 
 // Start the server
 app.listen(process.env.PORT || 4000, () => {
