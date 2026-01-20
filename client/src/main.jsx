7f5c108e2a9132ef09b10ab7425f1c994766850c
Zubair Abid <zubairabid54@gmail.com>
2026-01-20 21:35:47 +0500

    wrap app in redux provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,18 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
+import {Provider} from 'react-redux'
+import { store } from "./app/store.js";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 createRoot(document.getElementById("root")).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </ClerkProvider>
 );
